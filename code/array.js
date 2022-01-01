258dea851f01f2890517b2d5924591bac706cdee
Mario Gabriel PC <mario.contate.me@gmail.com>
2022-01-01 19:18:54 -0300

    exercicios dio

---
+++ code/array.js
@@ -0,0 +1,36 @@
+// const users = ["Guilherme", "pedro", "Jennifer", "Mario"]
+
+// const gender = {
+//     MAN = Symbol('M'),
+//     WOMAM = Symbol('W')
+// }
+
+// const persons = [
+//     {
+//         name: "Gilherme",
+//         age: 26,
+//         gender: gender.MAN
+//     },
+//     {
+//         name: "Pedro",
+//         age: 24,
+//         gender: gender.MAN
+//     },
+//     {
+//         name: "Jennifer",
+//         age: 28,
+//         gender: gender.WOMAM
+//     }
+// ];
+
+// // retorna a quantidade de linhas de um Arrey 
+// console.log('itens', person.length);
+
+// // Verificar se é arrey 
+// console.log('A variavel person é um Arrey:', Array.isArrey(persons));
+
+const dessert = { type: 'pie' }; 
+dessert.type = 'pudding';
+
+const seconds = dessert;
+seconds.type = 'fruit';
---
+++ code/spread.js
@@ -0,0 +1,9 @@
+let partes = ["ombro", "joelhos"];
+let musica = ['cabeca', ...partes, 'e', 'pés'];
+
+
+
+
+function fn(x, y, z){ }
+let args = [0, 1, 2];
+fn(...args);
